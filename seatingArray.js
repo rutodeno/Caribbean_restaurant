22703130bcc9b1f22b2f1700bd500a8255ffbab1
Dennis Ruto <rutodeno@gmail.com>
2018-05-01 22:29:05 -0400

    set up seating.html and seatingArray.js

---
+++ seatingArray.js
@@ -0,0 +1,10 @@
+var seating = [
+    {
+        tableNumber: 1,
+        name: "The crew",
+        phoneNumber: "111-111-1111",
+        email: "myguy@gmail.com"
+    }
+]
+
+module.exports = seating;
